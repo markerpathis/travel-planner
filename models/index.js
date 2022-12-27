ef61b795d9b7a9643dddf284949af49562260f6a
Parker Mathis <parkerjmathis@gmail.com>
2022-12-27 13:17:44 -0800

    Added many to many relationships for Traveller and Location

--- models/index.js
+++ models/index.js
@@ -3,9 +3,22 @@ const Location = require("./Location");
 const Traveller = require("./Traveller");
 const Trip = require("./Trip");
 
+// many to many relationship
+Traveller.belongsToMany(Location, {
+  through: {
+    model: Trip,
+    unique: false,
+  },
+  as: "planned_trips",
+});
 
-
-
+Location.belongsToMany(Traveller, {
+  through: {
+    model: Trip,
+    unique: false,
+  },
+  as: "location_travellers",
+});
 
 module.exports = {
   Location,
