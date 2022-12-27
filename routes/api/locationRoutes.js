66c7dc16aa16fa237882f982a41eaa45c1dd71f9
Parker Mathis <parkerjmathis@gmail.com>
2022-12-27 14:44:32 -0800

    Updated location routes

--- routes/api/locationRoutes.js
+++ routes/api/locationRoutes.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const sequelize = require("../../config/connection");
-const { Location } = require("../../models");
+const { Location, Traveller } = require("../../models");
 
 router.get("/", async (req, res) => {
   try {
+    const locaionData = await Location.findAll();
+    res.status(200).json(locaionData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -11,6 +13,14 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   try {
+    const locaionData = await Location.findByPk(req.params.id, {
+      include: [{ model: Traveller, as: "location_travellers" }],
+    });
+
+    if (!locaionData) {
+      res.status(404).json({ message: "No location found for this id" });
+    }
+    res.status(200).json(locaionData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -18,6 +28,8 @@ router.get("/:id", async (req, res) => {
 
 router.post("/", async (req, res) => {
   try {
+    const newLocation = await Location.create(req.body);
+    res.status(200).json(newLocation);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -25,6 +37,16 @@ router.post("/", async (req, res) => {
 
 router.delete("/:id", async (req, res) => {
   try {
+    const deletedLocation = await Location.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (deletedLocation) {
+      res.status(200).json(deletedLocation);
+    } else {
+      res.status(404).json({ message: "No location found with this id" });
+    }
   } catch (err) {
     res.status(500).json(err);
   }
