b98febc3e380e5458fd1450607ea8b1e0c8b6403
Parker Mathis <parkerjmathis@gmail.com>
2022-12-27 14:52:41 -0800

    Added trip routes

--- routes/api/tripRoutes.js
+++ routes/api/tripRoutes.js
@@ -2,8 +2,30 @@ const router = require("express").Router();
 const sequelize = require("../../config/connection");
 const { Trip } = require("../../models");
 
-router.post("/", (req, res) => {});
+router.post("/", async (req, res) => {
+  try {
+    const newTrip = await Trip.create(req.body);
+    res.status(200).json(newTrip);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedTrip = await Trip.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (deletedTrip) {
+      res.status(200).json(deletedTrip);
+    } else {
+      res.status(404).json({ message: "No trip found with this id" });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
